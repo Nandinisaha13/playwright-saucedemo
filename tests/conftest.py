78ef6e7887b703907e9f9b1bfe908565b2d2a14c
nandinisaha13 <nandinisaha13@gmail.com>
2026-03-28 23:53:33 +0530

    Add screenshot functionality for failed tests and update test_checkout assertions

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,15 @@ def page():
         yield page
         # print("Closing browser")
         browser.close()
+
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.failed:
+        page = item.funcargs.get("page")
+        if page:
+            page.screenshot(path=f"screenshots/{item.name}.png")
